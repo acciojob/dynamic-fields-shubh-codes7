f504a8fb0eed49031c8a6d3a8c55807449ee4fc5
shubh-codes7 <shubhamcodes7@gmail.com>
2025-04-20 17:46:20 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,48 @@
-
 import React from "react";
-import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+
+  const [fields, setFields] = React.useState([{id:1, name:"", age:""}])
+
+  function handleRemove(idToRemove){
+    setFields(fields.filter( field => field.id !== idToRemove))
+  }
+
+  function handleAddMore(){
+    setFields([...fields, {id: fields.length+1, name:"", age:""}] )
+  }
+
+  function handleChange(id, key, value){
+    const updatedFields = fields.map(field =>
+      field.id === id ? { ...field, [key]: value } : field
+    );
+    setFields(updatedFields);
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    console.log("Submitted!", fields)
+  }
+
+
+  return(
+    <form onSubmit={handleSubmit}>
+      {
+        fields.map(({id, name, age}) => {
+          return(
+            <div key={id}>
+              <input type="text" placeholder="Name" value = {name} onChange={(e) => handleChange(id, "name", e.target.value)}/>
+              <input type="text" placeholder="Age" value = {age} onChange={(e) => handleChange(id, "age", e.target.value)}/>
+              <button type="button" onClick={() => handleRemove(id)}>Remove</button>
+            </div>
+          )
+        })
+      }
+      <button type="button" onClick={handleAddMore}>Add more...</button>
+      <button type="submit" >Submit</button>
+    </form>
   )
 }
+  
 
-export default App
+export default App;
