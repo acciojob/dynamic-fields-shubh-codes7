4430289b4d29ad404bc0ec2772d27c5fb7fc2b01
shubh-codes7 <shubhamcodes7@gmail.com>
2025-04-20 19:18:49 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,61 @@
 import React from "react";
 
 const App = () => {
+  const [fields, setFields] = React.useState([{ id: 1, name: "", age: "" }]);
 
-  const [fields, setFields] = React.useState([{id:1, name:"", age:""}])
+  const handleAddMore = () => {
+    const newId = fields.length + 1;
+    setFields([...fields, { id: newId, name: "", age: "" }]);
+  };
 
-  function handleRemove(idToRemove){
-    setFields(fields.filter( field => field.id !== idToRemove))
-  }
+  const handleRemove = (idToRemove) => {
+    setFields(fields.filter((field) => field.id !== idToRemove));
+  };
 
-  function handleAddMore(){
-    setFields([...fields, {id: fields.length+1, name:"", age:""}] )
-  }
-
-  function handleChange(id, key, value){
-    const updatedFields = fields.map(field =>
+  const handleChange = (id, key, value) => {
+    const updated = fields.map((field) =>
       field.id === id ? { ...field, [key]: value } : field
     );
-    setFields(updatedFields);
-  }
+    setFields(updated);
+  };
 
-  function handleSubmit(e){
-    e.preventDefault()
-    console.log("Submitted!", fields)
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let dataToSubmit = fields.map(({name, age})=> ({name, age}))
+    console.log(dataToSubmit);
+  };
 
-
-  return(
+  return (
     <form onSubmit={handleSubmit}>
-      {
-        fields.map(({id, name, age}) => {
-          return(
-            <div key={id}>
-              <input type="text" placeholder="Name" name="name" value = {name} onChange={(e) => handleChange(id, "name", e.target.value)}/>
-              <input type="text" placeholder="Age" name="age" value = {age} onChange={(e) => handleChange(id, "age", e.target.value)}/>
-              <button type="button" onClick={() => handleRemove(id)}>Remove</button>
-            </div>
-          )
-        })
-      }
-      <button type="button" onClick={handleAddMore}>Add more...</button>
-      <button type="submit" >Submit</button>
+      <div id="form">
+        {fields.map(({ id, name, age }) => (
+          <div key={id} id={`form-${id}`}>
+            <input
+              id="name"
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => handleChange(id, "name", e.target.value)}
+            />
+            <input
+              id="age"
+              type="number"
+              placeholder="Age"
+              value={age}
+              onChange={(e) => handleChange(id, "age", e.target.value)}
+            />
+            <button type="button" onClick={() => handleRemove(id)}>
+              Remove
+            </button>
+          </div>
+        ))}
+      </div>
+      <button type="button" onClick={handleAddMore}>
+        Add More..
+      </button>
+      <button type="submit">Submit</button>
     </form>
-  )
-}
-  
+  );
+};
 
 export default App;
